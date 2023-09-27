f9569ae91f7e5a519c43e86256045e4934662673
Raviranjan Kumar <108695586+Raviy12@users.noreply.github.com>
2023-09-27 23:37:19 +0530

    Add files via upload
    init

---
+++ Calculator/Calculator/script.js
@@ -0,0 +1,29 @@
+let screen = document.getElementById('screen');
+button = document.querySelectorAll('button');
+let screenValue ='';
+for(item of button){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log('Button text is',buttonText)
+        if(buttonText=='x'){
+            buttonText ='*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText =='clear'){
+            screenValue ='';
+            screen.value = screenValue;
+        }
+        else if (buttonText =='del'){
+            screenValue ='';
+            screen.value = screenValue;
+        }
+        else if (buttonText =='='){
+            screen.value = eval(screenValue);
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
